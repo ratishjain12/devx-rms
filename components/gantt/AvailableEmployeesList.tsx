a1595ffec92e2b67eb4910407a8159a6eb1341fb
ratish jain <ratishjain@ratishs-MacBook-Air.local>
2025-01-14 20:31:11 +0530

    fixes

--- components/gantt/AvailableEmployeesList.tsx
+++ components/gantt/AvailableEmployeesList.tsx
@@ -85,18 +85,6 @@ export function AvailableEmployeesList({
                   </div>
                 </div>
               </div>
-              {employee.skills?.length > 0 && (
-                <div className="mt-2 flex flex-wrap gap-1">
-                  {employee.skills.map((skill, index) => (
-                    <span
-                      key={index}
-                      className="px-2 py-0.5 text-xs bg-blue-50 text-blue-700 rounded-full"
-                    >
-                      {skill}
-                    </span>
-                  ))}
-                </div>
-              )}
             </div>
           ))}
         </div>
